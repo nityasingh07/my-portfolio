65833fc769a80f1467d71b9a76c4cc3ccb16e498
Nitya Singh <itsnityasingh@gmail.com>
2025-11-14 21:55:37 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Projects from "./pages/projects";
+import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Skills from "./pages/skills";
 import experience from "./pages/experience"; 
-import projects from "./pages/projects";
+
 
 
 function App() {
